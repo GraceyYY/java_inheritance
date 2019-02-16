8cfd0f76c022689b096cb818ce3f5c83fd70c328
Grace <lunay0325@gmail.com>
2019-02-16 23:40:27 +0800

    Implement the Feline class which inherited from Animal class

---
+++ src/Feline.java
@@ -0,0 +1,9 @@
+public class Feline extends Animal {
+    public Feline(int age, double weight) {
+        super(age, weight);
+    }
+
+    public void meow() {
+        System.out.println("喵喵喵");
+    }
+}
